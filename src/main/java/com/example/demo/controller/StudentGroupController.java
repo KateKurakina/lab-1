31399fc57e095c46acc86195c6161c48aa92a5bf
katevolm <katevolmont@gmail.com>
2021-03-04 09:38:08 +0300

    lab-3

---
+++ src/main/java/com/example/demo/controller/StudentGroupController.java
@@ -0,0 +1,11 @@
+package com.example.demo.controller;
+
+import com.example.demo.dao.MarkJdbc;
+import com.example.demo.dao.StudentGroupJdbc;
+import com.example.demo.model.Student;
+import com.example.demo.model.StudentGroup;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class StudentGroupController {
+}
